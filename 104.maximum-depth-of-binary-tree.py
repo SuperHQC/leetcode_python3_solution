ffffa2981988917e0cfbd99f87f960eedefca8be
superHQC <hongqichao@gmail.com>
2022-07-20 19:29:36 +0800

    104.maximum-depth-of-binary-tree

--- 104.maximum-depth-of-binary-tree.py
+++ 104.maximum-depth-of-binary-tree.py
@@ -11,15 +11,48 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         # recursive
-        # time: log(n)
-        # space: log(n)
-        if not root:
-            return 0
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        # time: O(n)
+        # space: O(n) worest case, average O(logn)
+        # if not root:
+        #     return 0
+        # return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+        #iterative BFS
+        # if not root:
+        #     return 0
+        # maxDepth = 0
+        # q = deque()
+        # q.append((root, 1))
+        # while q:
+        #     ptr, depth = q.popleft()
+        #     maxDepth = max(maxDepth, depth)
+        #     if ptr.left:
+        #         q.append((ptr.left, depth + 1))
+        #     if ptr.right:
+        #         q.append((ptr.right, depth + 1))
+
+        # return maxDepth
         
+        #iterative DFS
+        maxDepth = 0
+        if not root:
+            return maxDepth
+        stack = [(root, 1)]
+        while stack:
+            ptr, depth = stack.pop()
+            maxDepth = max(maxDepth, depth)
+            if ptr.left:
+                stack.append((ptr.left, depth + 1))
+            if ptr.right:
+                stack.append((ptr.right, depth + 1))
+            
+        return maxDepth
         
 # @lc code=end
 
