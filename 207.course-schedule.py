a301469284375ca50816c33ce64a8a39f54ae992
superHQC <hongqichao@gmail.com>
2022-07-23 14:33:19 +0800

    update

--- 207.course-schedule.py
+++ 207.course-schedule.py
@@ -8,23 +8,23 @@
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        todo = {i: set() for i in range(numCourses)}
+        indegree = {i: set() for i in range(numCourses)}
         graph = defaultdict(set)
-        q = deque()
         for i, j in prerequisites:
-            todo[i].add(j)
+            indegree[i].add(j)
             graph[j].add(i)
-        for k, v in todo.items():
+        q = deque()
+        for k, v in indegree.items():
             if len(v) == 0:
                 q.append(k)
         while q:
-            n = q.popleft()
-            for i in graph[n]:
-                todo[i].remove(n)
-                if len(todo[i]) == 0:
-                    q.append(i)
-            todo.pop(n)
-        return not todo
+            node = q.popleft()
+            for n in graph[node]:
+                indegree[n].remove(node)
+                if len(indegree[n]) == 0:
+                    q.append(n)
+            indegree.pop(node)
+        return not indegree
 
         
 # @lc code=end
