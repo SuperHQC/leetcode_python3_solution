8eb6f78bcbcdb441bd929f4ad4be2a691ff97b59
SuperHQC <hongqichao@gmail.com>
2022-08-08 00:36:11 +0800

    easy

---
+++ 1502.can-make-arithmetic-progression-from-sequence.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1502 lang=python3
+#
+# [1502] Can Make Arithmetic Progression From Sequence
+#
+
+# @lc code=start
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        
+# @lc code=end
+
